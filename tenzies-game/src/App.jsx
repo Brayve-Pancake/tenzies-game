af8a9d4f76635d1a97650fa6825a6f49b4ed2fb1
Joshua Bray <josh.c.bray@gmail.com>
2022-07-19 15:49:38 +0100

    Set up state for dice

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import Content from "./components/Content";
 import Die from "./components/Die";
 
 export default function App() {
-  // const [dice, setDice] = useState([]);
+  const [dice, setDice] = useState(allNewDice());
+  console.log(dice);
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
       newDice.push(Math.floor(Math.random() * 6) + 1);
     }
-    console.log(newDice[0]);
+    return newDice;
   }
 
   return (
