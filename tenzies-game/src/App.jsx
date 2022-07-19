7dc840a388ee815bfbb4309606a156ad0a2b333a
Joshua Bray <josh.c.bray@gmail.com>
2022-07-19 14:39:49 +0100

    Setup and sanity check connections

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,3 +1,13 @@
+import Button from "./components/Button";
+import Content from "./components/Content";
+import GameBoard from "./components/GameBoard";
+
 export default function App() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <Content />
+      <Button />
+      <GameBoard />
+    </div>
+  );
 }
---
+++ tenzies-game/src/components/GameBoard.jsx
@@ -0,0 +1,3 @@
+export default function GameBoard() {
+  return <h3>GameBoard</h3>;
+}
